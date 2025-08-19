4433a5577f47281462a8ee91dce442f0f42571ea
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-08-19 08:50:36 -0300

    ✨ Setup rough.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import rough from 'roughjs';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const hasInitialized = useRef(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const container = containerRef.current;
+
+    if (!canvas || !container || hasInitialized.current) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+
+    if (!ctx) {
+      return;
+    }
+
+    hasInitialized.current = true;
+
+    const width = container.clientWidth;
+    const height = container.clientHeight;
+
+    canvas.style.width = width + 'px';
+    canvas.style.height = height + 'px';
+    canvas.width = width * window.devicePixelRatio;
+    canvas.height = height * window.devicePixelRatio;
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+    const rc = rough.canvas(canvas);
+    rc.rectangle(10, 10, 200, 200, {
+      fill: 'blue',
+    });
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      ref={containerRef}
+      className="w-screen h-screen overflow-hidden bg-gray-50"
+    >
+      <canvas ref={canvasRef} />
+    </div>
+  );
 }
 
-export default App
+export default App;
