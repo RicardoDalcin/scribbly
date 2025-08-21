28824e9f08863f0b001a67f71108659d9853e24b
Ricardo Dalcin <ricardo.dalcinh@gmail.com>
2025-08-21 08:14:09 -0300

    :sparkles: Setup Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef } from 'react';
-import rough from 'roughjs';
+import { useEffect, useRef } from "react";
+import rough from "roughjs";
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -14,7 +14,7 @@ function App() {
       return;
     }
 
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext("2d");
 
     if (!ctx) {
       return;
@@ -25,24 +25,30 @@ function App() {
     const width = container.clientWidth;
     const height = container.clientHeight;
 
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
     canvas.width = width * window.devicePixelRatio;
     canvas.height = height * window.devicePixelRatio;
     ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
 
     const rc = rough.canvas(canvas);
     rc.rectangle(10, 10, 200, 200, {
-      fill: 'blue',
+      fill: "blue",
     });
   });
 
   return (
     <div
       ref={containerRef}
-      className="w-screen h-screen overflow-hidden bg-gray-50"
+      className="w-screen h-screen overflow-hidden bg-white relative"
     >
       <canvas ref={canvasRef} />
+
+      <div className="abolute top-6 left-1/2 -trnaslate-x-1/2 shadow-md border border-neutral-200 rounded-2xl bg-white/90 backdrop-blur-lg">
+        <div className="flex items-center px-3 py-2 gap-2">
+      
+        </div>
+      </div>
     </div>
   );
 }
